643fff15f6910bd665e1c583e759b01298ca56e4
fahd01 <elgayed.fahd@esprit.tn>
2025-02-13 23:25:40 +0100

    initial commit

---
+++ src/main/java/tn/esprit/studdycoursemanagmentmicroservice/StuddyCourseManagmentMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.studdycoursemanagmentmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuddyCourseManagmentMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StuddyCourseManagmentMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/studdycoursemanagmentmicroservice/StuddyCourseManagmentMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.studdycoursemanagmentmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuddyCourseManagmentMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
