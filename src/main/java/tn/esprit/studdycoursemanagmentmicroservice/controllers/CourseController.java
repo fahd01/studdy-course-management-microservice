0713c3288c055b7cb0350f3be634147923177d68
Fahd Elgayed <elgayed.fahd@esprit.tn>
2025-02-25 11:53:45 +0100

    Add course pagination

--- src/main/java/tn/esprit/studdycoursemanagmentmicroservice/controllers/CourseController.java
+++ src/main/java/tn/esprit/studdycoursemanagmentmicroservice/controllers/CourseController.java
@@ -1,6 +1,10 @@
 package tn.esprit.studdycoursemanagmentmicroservice.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.studdycoursemanagmentmicroservice.entities.Course;
 import tn.esprit.studdycoursemanagmentmicroservice.services.CourseService;
@@ -16,6 +20,20 @@ public class CourseController {
     public List<Course> getAll(){
         return courseService.getAll();
     }
+
+    @GetMapping("/paged")
+    public Page<Course> getAllPaged(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "true") boolean ascending
+    ) {
+        Sort sort = ascending ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return courseService.paginateCourses(pageable);
+    }
+
+
     @GetMapping("/{id}")
     public Course getById(@PathVariable long id){return courseService.getById(id);}
     @PostMapping
